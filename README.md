eac9dda2b59a13431fc4bd404e51905c3f575bf8
Thom David <thomdavid74@gmail.com>
2018-04-11 12:37:14 -0700

    Sample data stucture, controller test

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TRACALORIE APP
 An app that tracks meal and calorie content, written in pure Javascript, using the Javascript module pattern. MaterializeCSS is used for the UI, which requires jQuery and is the sole reason for its inclusion in the project. Tracalorie is a browser-based app that uses local storage to persist the data from session to session. The app permits items to be edited/updated based upon a changed state.
 
-The module schema for the app:
+The controller schema for the app:
 * Storage Controller
 * Item Controller
 * UI Controller
--- app.js
+++ app.js
@@ -1,16 +1,60 @@
 // Storage Controller here
 
-// Item Controller IIFE
-const ItemCtrl = (function() {
 
+
+
+// Item Controller IIFE  *****************************
+const ItemCtrl = (function() {
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Burger Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 200 },
+      { id: 2, name: 'Pizza', calories: 850 }
+    ],
+    currentItem: null,
+    totatCalories: 0
+  }
+  // Public methods
+  return {
+    logData: function() {
+      return data;
+    }
+  }
 })();
 
-// UI Controller
+
+
+
+// UI Controller  ******************************
 const UICtrl = (function() {
 
+  // Public methods
+  return {
+
+  }
  })();
 
-// App Controller
+
+
+// App Controller *******************************
 const App = (function(ItemCtrl, UICtrl) {
+  
+  // Public methods
+  return {
+    init: function() {
+      console.log('initializing app...'); // testing only
+    }
+  }
 
  })(ItemCtrl, UICtrl);
+
+
+ // Initialize app
+ App.init();
