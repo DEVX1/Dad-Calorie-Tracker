b1b06d907c754d5289bb2ad6f53d1559d3bc3da3
DEVX1 <38556505+DEVX1@users.noreply.github.com>
2018-10-10 12:15:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ const UICtrl = (function() {
       // loop through the list of items
       items.forEach(function(item) {
         html += `<li class="collection-item" id="item-${item.id}">
-        <strong>${item.name}:</strong> <em>${item.calories} ISBN Number</em>
+        <strong>${item.name}:</strong> <em>${item.calories} Calories</em>
         <a href="#" class="secondary-content"><i class="fa fa-pencil edit-item"></i></a>
       </li>`;
       });
@@ -254,7 +254,7 @@ const UICtrl = (function() {
         const itemID = listItem.getAttribute('id');
 
         if(itemID === `item-${item.id}`) {
-          document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}:</strong> <em>${item.calories} ISBN Number</em>
+          document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}:</strong> <em>${item.calories} Calories</em>
           <a href="#" class="secondary-content"><i class="fa fa-pencil edit-item"></i></a>`;
         }
       });
